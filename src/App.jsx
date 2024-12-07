f012f88ffbe981208c6db2cfcb24b2613cf3646c
Jay Cortez Jr. <119042660+Frijaaay@users.noreply.github.com>
2024-12-07 09:59:05 +0000

    update info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jay Cortez Jr.",
+  title: "4th Year Information Technology Student",
+  email: "jr.cortezjay@gmail.com",
+  gitHub: "https://github.com/Frijaaay",
+  instagram: "https://www.instagram.com/_frijayy/",
+  linkedIn: "https://www.linkedin.com/in/jr-cortez-jay/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
